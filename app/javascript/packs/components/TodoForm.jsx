bd59cbb014e5ffc76a42ec541a0664c9918af50f
Cedric Johnson <cjohns0913@gmail.com>
2021-04-17 21:36:06 -0500

    Add axios headers for CSRF protection

--- app/javascript/packs/components/TodoForm.jsx
+++ app/javascript/packs/components/TodoForm.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import axios from "axios";
+import setAxiosHeaders from "./AxiosHeaders";
+
 class TodoForm extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +13,7 @@ class TodoForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    setAxiosHeaders();
     axios
       .post("/api/v1/todo_items", {
         todo_item: {
