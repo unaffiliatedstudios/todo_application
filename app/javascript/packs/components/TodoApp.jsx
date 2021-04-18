1fbf76abda2856edcaaf83da1be9644d5c203979
Cedric Johnson <cjohns0913@gmail.com>
2021-04-17 21:32:30 -0500

    Import TodoForm

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 import TodoItems from "./TodoItems";
 import TodoItem from "./TodoItem";
+import TodoForm from "./TodoForm";
 
 class TodoApp extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class TodoApp extends React.Component {
       todoItems: [],
     };
     this.getTodoItems = this.getTodoItems.bind(this);
+    this.createTodoItem = this.createTodoItem.bind(this);
   }
   componentDidMount() {
     this.getTodoItems();
@@ -29,13 +31,21 @@ class TodoApp extends React.Component {
       });
   }
 
+  createTodoItem(todoItem) {
+    const todoItems = [todoItem, ...this.state.todoItems];
+    this.setState({ todoItems });
+  }
+
   render() {
     return (
-      <TodoItems>
-        {this.state.todoItems.map((todoItem) => (
-          <TodoItem key={todoItem.id} todoItem={todoItem} />
-        ))}
-      </TodoItems>
+      <>
+        <TodoForm createTodoItem={this.createTodoItem} />
+        <TodoItems>
+          {this.state.todoItems.map((todoItem) => (
+            <TodoItem key={todoItem.id} todoItem={todoItem} />
+          ))}
+        </TodoItems>
+      </>
     );
   }
 }
