d5fa7f5d6c366399215379527010afe5b65f1207
Cedric Johnson <cjohns0913@gmail.com>
2021-04-17 21:00:04 -0500

    Authorization for update action

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -28,6 +28,17 @@ class Api::V1::TodoItemsController < ApplicationController
       end
     end
     def update
+      if authorized?
+          respond_to do |format|
+            if @todo_item.update(todo_item_params)
+              format.json { render :show, status: :ok, location: api_v1_todo_item_path(@todo_item) }
+            else
+              format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+            end
+          end
+      else
+          handle_unauthorized
+      end
     end
     def destroy
     end
