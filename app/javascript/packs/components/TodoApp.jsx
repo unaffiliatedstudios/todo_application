724ecd1511009aa20fa5a87492cde4d588bd7ec7
Cedric Johnson <cjohns0913@gmail.com>
2021-04-17 22:50:31 -0500

    Add ErrorMessage to app

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -6,6 +6,7 @@ import TodoItems from "./TodoItems";
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 class TodoApp extends React.Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class TodoApp extends React.Component {
       todoItems: [],
       hideCompletedTodoItems: false,
       isLoading: true,
+      errorMessage: null,
     };
     this.getTodoItems = this.getTodoItems.bind(this);
     this.createTodoItem = this.createTodoItem.bind(this);
@@ -52,6 +54,9 @@ class TodoApp extends React.Component {
   render() {
     return (
       <>
+        {this.state.errorMessage && (
+          <ErrorMessage errorMessage={this.state.errorMessage} />
+        )}
         {!this.state.isLoading && (
           <>
             <TodoForm createTodoItem={this.createTodoItem} />
