bf9a12f4c0a88f5f1ecc82e6a0377517653ff99b
Cedric Johnson <cjohns0913@gmail.com>
2021-04-17 21:41:39 -0500

    Sort out delete function with items

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,6 +1,8 @@
 // app/javascript/packs/components/TodoItem.jsx
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import setAxiosHeaders from "./AxiosHeaders";
 
 class TodoItem extends React.Component {
   constructor(props) {
@@ -8,7 +10,25 @@ class TodoItem extends React.Component {
     this.state = {
       complete: this.props.todoItem.complete,
     };
+    this.handleDestroy = this.handleDestroy.bind(this);
+    this.path = `/api/v1/todo_items/${this.props.todoItem.id}`;
   }
+
+  handleDestroy() {
+    setAxiosHeaders();
+    const confirmation = confirm("Are you sure?");
+    if (confirmation) {
+      axios
+        .delete(this.path)
+        .then((response) => {
+          this.props.getTodoItems();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
   render() {
     const { todoItem } = this.props;
     return (
@@ -61,7 +81,12 @@ class TodoItem extends React.Component {
               Complete?
             </label>
           </div>
-          <button className="btn btn-outline-danger">Delete</button>
+          <button
+            onClick={this.handleDestroy}
+            className="btn btn-outline-danger"
+          >
+            Delete
+          </button>
         </td>
       </tr>
     );
@@ -72,4 +97,5 @@ export default TodoItem;
 
 TodoItem.propTypes = {
   todoItem: PropTypes.object.isRequired,
+  getTodoItems: PropTypes.func.isRequired,
 };
